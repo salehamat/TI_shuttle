4bb314c13f3b0edcf0153bfb129f4dd7c6da3972
MANAN SHAH <a0230478@ti.com>
2019-05-21 18:10:59 +0530

    second commit

--- app/src/main/java/com/example/ti_shuttle/MainActivity.java
+++ app/src/main/java/com/example/ti_shuttle/MainActivity.java
@@ -1,24 +1,114 @@
 package com.example.ti_shuttle;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
+import android.provider.Settings;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.lang.ref.WeakReference;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 import java.util.Set;
+class Mapping {
+    private Passenger passenger;
+    private String cardId;
 
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(Passenger passenger) {
+        this.passenger = passenger;
+    }
+
+    public String getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(String cardId) {
+        this.cardId = cardId;
+    }
+}
+
+
+class Passenger {
+    private String AID;
+    private String locationIn;
+    private String timeIn;
+    private String locationOut;
+    private String timeOut;
+    private String Date;
+
+    public String getAID() {
+        return AID;
+    }
+
+    public void setAID(String AID) {
+        this.AID = AID;
+    }
+
+    public String getLocationIn() {
+        return locationIn;
+    }
+
+    public void setLocationIn(String locationIn) {
+        this.locationIn = locationIn;
+    }
+
+    public String getTimeIn() {
+        return timeIn;
+    }
+
+    public void setTimeIn(String timeIn) {
+        this.timeIn = timeIn;
+    }
+
+    public String getLocationOut() {
+        return locationOut;
+    }
+
+    public void setLocationOut(String locationOut) {
+        this.locationOut = locationOut;
+    }
+
+    public String getTimeOut() {
+        return timeOut;
+    }
+
+    public void setTimeOut(String timeOut) {
+        this.timeOut = timeOut;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+}
+
+@RequiresApi(api = Build.VERSION_CODES.O)
 public class MainActivity extends AppCompatActivity {
 
     /*
@@ -48,6 +138,10 @@ public class MainActivity extends AppCompatActivity {
     };
     private UsbService usbService;
     private TextView display;
+    private LocationManager locationManager;
+    private LocationListener listener;
+    private String data;
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 
     private MyHandler mHandler;
     private final ServiceConnection usbConnection = new ServiceConnection() {
@@ -63,6 +157,25 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private String getLocation(Location location){
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        if (lat > 12.977866 && lat < 12.979581 && lng > 77.659198 && lng < 77.659949){
+            return "TI main Building";
+        }
+        else if (lat > 12.977919 && lat < 12.979497 && lng > 77.660292 && lng < 77.661075){
+            return "Lake view Building";
+        }
+        else if (lat > 12.984671 && lat < 12.987117 && lng > 77.643493 && lng < 77.646948){
+            return "SVM";
+        }
+        else if (lat > 12.996535 && lat < 13.0265826 && lng > 77.668979 && lng < 77.681981){
+            return "Tin Factory";
+        }
+        else{
+            return "Not Identified";
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,6 +184,37 @@ public class MainActivity extends AppCompatActivity {
         mHandler = new MyHandler(this);
 
         display = findViewById(R.id.textView1);
+        // ToDo: sleep thread
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location loc) {
+                String location = getLocation(loc);
+                //display.append(data + ": " + location.getLongitude() + " " + location.getLatitude() + ": " + dtf.format(LocalDateTime.now()));
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+        /*if (usbService.serialPortConnected) { // if UsbService was correctly binded, Send data
+            usbService.write(new byte[]{(byte) 0xfe, (byte) 0x80, (byte) 0x00}); //initialization data
+        } else {
+            Toast.makeText(getApplicationContext(), "No devices connected", Toast.LENGTH_LONG).show();
+        }*/
     }
 
     @Override
@@ -124,7 +268,7 @@ public class MainActivity extends AppCompatActivity {
     /*
      * This handler will be passed to UsbService. Data received from serial port is displayed through this handler
      */
-    private static class MyHandler extends Handler {
+    private class MyHandler extends Handler {
         private final WeakReference<MainActivity> mActivity;
 
         MyHandler(MainActivity activity) {
@@ -135,9 +279,15 @@ public class MainActivity extends AppCompatActivity {
         public void handleMessage(Message msg) {
             switch (msg.what) {
                 case UsbService.MESSAGE_FROM_SERIAL_PORT:
-                    String data = (String) msg.obj;
-                    mActivity.get().display.append(data + "\n");
+                    data = (String) msg.obj;
+                    //mActivity.get().display.append(data + "\n");
                     // ToDo: New code to be put here
+
+                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}, 10);
+                    }
+                    locationManager.requestLocationUpdates("gps", 5000, 100, listener);
+
                     break;
                 case UsbService.CTS_CHANGE:
                     Toast.makeText(mActivity.get(), "CTS_CHANGE", Toast.LENGTH_LONG).show();
---
+++ app/src/main/java/com/example/ti_shuttle/WorkingActivity.java
@@ -0,0 +1,13 @@
+package com.example.ti_shuttle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_working);
+    }
+}
